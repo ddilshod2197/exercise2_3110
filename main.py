efba40fa96c5eba5ea7a9c78ec6e9c441c143a25
ddilshod2197 <ddilshod2197@gmail.com>
2025-10-31 15:11:21 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ class Televizor:
         self.kanal = kanal
 
     def ovoz_oshir(self, miqdor=1):
-        """Ovozni oshiradi"""
         self.ovoz += miqdor
         print(f"Ovoz darajasi: {self.ovoz}")
 
     def kanal_ozgartir(self, yangi_kanal):
-        """Kanalni o'zgartiradi"""
         self.kanal = yangi_kanal
         print(f"Yangi kanal: {self.kanal}")
+
+
 
 tv1 = Televizor("Samsung", 10, 5)
 print(f"Brend: {tv1.brend}, Ovoz: {tv1.ovoz}, Kanal: {tv1.kanal}")
